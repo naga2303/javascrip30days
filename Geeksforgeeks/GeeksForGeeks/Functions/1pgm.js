8ecf8aa0ec391888f912c46f5c348766ae27a336
Nagarajan <nagaraj2319@gmail.com>
2023-11-22 10:05:09 +0530

    Functions added

---
+++ Geeksforgeeks/GeeksForGeeks/Functions/1pgm.js
@@ -0,0 +1,22 @@
+//How to write a function in JavaScript ?
+
+//Function definition
+function test(){
+    console.log()
+}
+
+//function definition
+let a = function (t){
+    console.log()
+}
+
+//Arrow function
+let x =()=>{
+
+}
+
+//unnamed function
+let y = fuction ()
+{
+    console.log("test")
+}
---
+++ Geeksforgeeks/GeeksForGeeks/Functions/2pgm.js
@@ -0,0 +1,6 @@
+//What is currying function in JavaScript ?
+/**
+ * It is a technique in function programming, that tranforms the function of multiple arguments into several functions 
+ * of a single argument in sequence. 
+ */
+console.log()
