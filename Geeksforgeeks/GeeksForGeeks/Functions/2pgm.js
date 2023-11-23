16addca91d54056c6731d23008a40b894059c086
nagarajan <nagarajan.r@volantetech.com>
2023-11-23 14:18:44 +0530

    Functions added

--- Geeksforgeeks/GeeksForGeeks/Functions/2pgm.js
+++ Geeksforgeeks/GeeksForGeeks/Functions/2pgm.js
@@ -3,4 +3,13 @@
  * It is a technique in function programming, that tranforms the function of multiple arguments into several functions 
  * of a single argument in sequence. 
  */
-console.log()
+console.log(calsValue(3)(4)(5))
+function calsValue(length)
+{
+    return function(breadth){
+        return function (height)
+        {
+            return length*breadth*height
+        }
+    }
+}
