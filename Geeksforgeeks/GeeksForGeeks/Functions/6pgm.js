1e915019ede03f630c8a6da53739a95319fa72be
nagarajan <nagarajan.r@volantetech.com>
2023-11-23 15:25:22 +0530

    functionPgms added

---
+++ Geeksforgeeks/GeeksForGeeks/Functions/6pgm.js
@@ -0,0 +1,16 @@
+//How to check whether a number is NaN or finite in JavaScript ?
+ console.log(isNaN('Naga'))  //true
+
+ console.log(isNaN('23')) //false
+
+ console.log(isNaN(34)) //false
+
+ /**
+  * isFinite() = To determine whether the number is finite.
+  */
+
+ console.log(isFinite(34)) //true
+
+ console.log(isFinite('23/03/2023')) //false
+
+ console.log(isFinite('34')) //true
