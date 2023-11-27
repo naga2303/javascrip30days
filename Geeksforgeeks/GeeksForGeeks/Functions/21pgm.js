0ad9a7dbda1650296f6d8c3a9b371b0fc55f3114
nagarajan <nagarajan.r@volantetech.com>
2023-11-27 17:27:15 +0530

    Fnctions pgm added

---
+++ Geeksforgeeks/GeeksForGeeks/Functions/21pgm.js
@@ -0,0 +1,9 @@
+//https://www.geeksforgeeks.org/how-to-check-a-function-is-defined-in-javascript/
+/**
+ * typeof() - this operator is used to find the type of the variable.
+ * synatx: typeof var
+ */
+var x = function(){
+    return true;
+}
+console.log(typeof x)
