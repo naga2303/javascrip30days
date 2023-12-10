d60af29d550c626aa47e03f36c7fbb7c9334b936
Nagarajan <nagaraj2319@gmail.com>
2023-12-10 19:01:08 +0530

    More pgms

---
+++ Geeksforgeeks/GeeksForGeeks/Number/2pgm.js
@@ -0,0 +1,11 @@
+//How to create a Number object using JavaScript ?
+
+let n = Number(val)
+
+/**The number function is used to convert any data-type to number */
+
+let num = Number.MAX_VALUE
+console.log(num)
+
+let smallNum = Number.MIN_VALUE
+console.log(smallNum)
---
+++ Geeksforgeeks/GeeksForGeeks/Number/4pgm.js
@@ -0,0 +1,5 @@
+//How to add float numbers using JavaScript ?
+
+let a = parseFloat('2.3')
+let b = parseFloat('3.3')
+console.log((a+b).toFixed(2))
