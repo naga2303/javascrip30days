21824083bf836519540bd608d04fc5aebb165984
nagarajan <nagaraj2319@gmail.com>
2024-07-29 20:48:41 +0530

    Test

--- Javapgms/src/APAC/Strings/LongSubWithoutRepeating.java
+++ Javapgms/src/APAC/Strings/LongSubWithoutRepeating.java
@@ -1,41 +1,30 @@
 package APAC.Strings;
-public class LongSubWithoutRepeating
-{
-    public static void main(String args[]){
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongSubWithoutRepeating {
+    public static void main(String args[]) {
         String s1 = "abcabcbb";
         String s2 = "bbbb";
         String s3 = "pwwkew";
-        LongSubWithoutRepeating.longRepeating(s1);
-        LongSubWithoutRepeating.longRepeating(s2);
-        LongSubWithoutRepeating.longRepeating(s3);
+        System.out.println(LongSubWithoutRepeating.longRepeating(s1));
+        System.out.println(LongSubWithoutRepeating.longRepeating(s2));
+        System.out.println(LongSubWithoutRepeating.longRepeating(s3));
+
+
     }
-    public static int longRepeating(String word)
-    {
-        String sub = "";
-        int len=0;
 
-        for(int i=0;i<word.length();i++){
-            String temp = "";
-
-            if(temp.length()==0){
-                temp=word.substring(i, i);
+    public static int longRepeating(String word) {
+        int n = word.length(), ans = 0;
+        Map<Character, Integer> mp = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (mp.containsKey(word.charAt(j))) {
+                i = Math.max(mp.get(word.charAt(j)), i);
             }
-            if(!(word.substring(i, i)).contains(temp))
-            {
-                temp=temp+word.charAt(i);
-            }
-            else
-            {
-                continue;
-            }
-            if(i==word.length()-1)
-            {
-                sub= temp;
-                len = sub.length();
-            }
-
+            ans = Math.max(ans, j - i + 1);
+            mp.put(word.charAt(j), j + 1);
         }
-
-        return len;
+        return ans;
     }
 }
--- Javapgms/src/TestLeaf/TraceOutput.java
+++ Javapgms/src/TestLeaf/TraceOutput.java
@@ -2,10 +2,5 @@ package TestLeaf;
 
 public class TraceOutput
 {
-    public static void main(String args[]){
-        for (int index =0;1;index++){
-            System.out.println("Welcome");
-            break;
-        }
-    }
+
 }
